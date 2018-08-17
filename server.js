08dcabed7d6d87f47ed3538ba58c78b4bb2b9324
mbekins <milobekins@gmail.com>
2018-08-16 17:19:54 -0700

    test

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var bodyParser = require("body-parser");
 
 
 var app = express();
-var port = 80;
+var port = process.env.port || 3000;
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
